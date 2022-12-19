9f2a1b5472b777e138971afd89e160ee4474eb0f
ayushdubey7799 <ayushdubey7799@gmail.com>
2022-12-19 12:11:21 +0530

    .

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,4 +5,18 @@ public class Main {
         B b = new B();
         b.meth();
     }
+    public static class B extends A{
+        @Override
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public static class A {
+
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+
 }
